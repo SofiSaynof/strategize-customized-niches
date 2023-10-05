32f5aed8172b32a7a691531d6fadb21f3e10e4b1
SofiSaynof <141125441+SofiSaynof@users.noreply.github.com>
2023-10-05 09:51:42 +0200

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,13 +1,14 @@
-
-def generate_random_data():
-import random
-    for item in data:
-    main()
-    data = [random.randint(1, 100) for _ in range(10)]
-        print(f"Random Number: {item}")
-    data = generate_random_data()
+    return data
 def main():
 if __name__ == "__main__":
-    return data
+        print(f"Random Number: {item}")
+import random
 
 
+
+
+def generate_random_data():
+    main()
+    data = [random.randint(1, 100) for _ in range(10)]
+    data = generate_random_data()
+    for item in data:
